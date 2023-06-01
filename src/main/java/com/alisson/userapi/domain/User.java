0ee7164c1370d75d863b15ddb67288634f229215
alissondevsouza <alisson.rhuan@crearesistemas.com.br>
2023-06-01 10:15:13 -0300

    Feat: start project

---
+++ src/main/java/com/alisson/userapi/domain/User.java
@@ -0,0 +1,74 @@
+package com.alisson.userapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String userEmail;
+    private String userLogin;
+    private String userPassword;
+
+    public User() {
+
+    }
+    public User(Long id, String userName, String userEmail, String userLogin, String userPassword) {
+        this.id = id;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userLogin = userLogin;
+        this.userPassword = userPassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+}
