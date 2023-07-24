d034d891b73099f928783d84852fe18961f5a199
alissondevsouza <alisson.rhuan@crearesistemas.com.br>
2023-07-24 10:51:07 -0300

    Fix: Ajuste em metodo de montagem de token

--- src/main/java/com/alisson/userapi/security/SecurityFilter.java
+++ src/main/java/com/alisson/userapi/security/SecurityFilter.java
@@ -44,6 +44,6 @@ public class SecurityFilter extends OncePerRequestFilter {
             return null;
         }
 
-        return authHeader.replace("Bearer", "");
+        return authHeader.replace("Bearer ", "");
     }
 }
