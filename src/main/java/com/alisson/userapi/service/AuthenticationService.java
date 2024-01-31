aca6326dc324db566783cfdcf5f402c329e52ec8
alissondevsouza <alisson.rhuan@crearesistemas.com.br>
2024-01-31 16:51:56 -0300

    Refactor: Refactored Authentication Service layer

--- src/main/java/com/alisson/userapi/service/AuthenticationService.java
+++ src/main/java/com/alisson/userapi/service/AuthenticationService.java
@@ -1,7 +1,8 @@
 package com.alisson.userapi.service;
 
-import com.alisson.userapi.domain.Dtos.AuthenticationDto;
-import com.alisson.userapi.domain.entity.User;
+import com.alisson.userapi.domain.auth.AuthenticationDTO;
+import com.alisson.userapi.domain.user.User;
+import com.alisson.userapi.exceptionHandling.exceptions.LoginNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,11 +15,15 @@ public class AuthenticationService {
     @Autowired
     private TokenService tokenService;
 
-    public String login(AuthenticationDto authenticationDto) {
+    public String login(AuthenticationDTO authenticationDto) {
 
-        var usernamePassword = new UsernamePasswordAuthenticationToken(authenticationDto.userLogin(), authenticationDto.userPassword());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-
-        return tokenService.generateToken((User) auth.getPrincipal());
+        var usernamePassword =
+                new UsernamePasswordAuthenticationToken(authenticationDto.userLogin(), authenticationDto.userPassword());
+        try{
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            return tokenService.generateToken((User) auth.getPrincipal());
+        }catch (Exception e) {
+            throw new LoginNotFoundException("Login or password invalid");
+        }
     }
 }
